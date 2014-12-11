becd10d1de80fd2ba0d7e9d730f8f40c7ecde719
joamag <joamag@gmail.com>
2014-12-11 16:03:13 +0000

    new route

--- src/digitalriver/controllers/base.py
+++ src/digitalriver/controllers/base.py
@@ -9,3 +9,7 @@ class BaseController(appier.Controller):
     @appier.route("/index", "GET")
     def index(self):
         return self.template("index.html.tpl")
+
+    @appier.route("/deploy", ("GET", "POST"))
+    def deploy(self):
+        pass
