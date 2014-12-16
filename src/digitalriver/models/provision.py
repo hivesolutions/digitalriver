e2151728007240d8639e83f113a297433e1da6b8
joamag <joamag@gmail.com>
2014-12-16 16:47:14 +0000

    new pushi support

--- src/digitalriver/models/provision.py
+++ src/digitalriver/models/provision.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import uuid
+import threading
 
+import pushi
 import appier
 
 from . import base
@@ -54,13 +56,36 @@ class Provision(base.DRBase):
 
     def post_create(self):
         base.DRBase.post_create(self)
-        self.deploy()
+        thread = threading.Thread(target = self.deploy)
+        thread.start()
 
     def deploy(self):
-        api = self.owner.get_api()
-        if not api: return
+        logger = self.create_logger()
         deployer = self.owner.get_deployer(
             address = self.droplet_address,
             username = "root"
         )
+        deployer.bind("stdout", logger)
         deployer.deploy_url(self.url)
+
+    def create_logger(self):
+        pushi_logger = self.create_pushi()
+
+        def logger(message):
+            pushi_logger(message)
+
+        return logger
+
+    def create_pushi(self):
+
+        def on_connect(connection):
+            connection.subscribe_pushi(self.id)
+
+        client_key = appier.conf("PUSHI_KEY")
+        client = pushi.PushiClient(client_key = client_key)
+        connection = client.connect_pushi(callback = on_connect)
+
+        def logger(message):
+            connection.send_channel("stdout", message, self.id, persist = False)
+
+        return logger
