117eb6aa76d53ce67b1bc5367a7a7a4627a92e78
joamag <joamag@gmail.com>
2014-12-24 11:55:47 +0000

    better deployer support

--- src/digitalriver/util/deployer.py
+++ src/digitalriver/util/deployer.py
@@ -13,6 +13,14 @@ except: paramiko = None
 
 class Deployer(appier.Observable):
 
+    CONFIG_FILE = "config.env"
+    """ The name of the file that is going to be used to store the
+    bash related variable exporting, to be used during runtime """
+
+    DATA_DIRECTORY = "/data"
+    """ The directory where all the ephemeral/persistent data will
+    be stored and it's considered the state of the machine """
+
     BASE_DIRECTORY = "/torus"
     """ The base directory where the instance configuration of the
     torus infra-structure will be positioned for execution """
@@ -71,7 +79,9 @@ class Deployer(appier.Observable):
             self.deploy_url(dependency)
 
         self.run_base()
+        self.run_config()
         self.run_script(build)
+        self.close_ssh()
 
         instance.features.append(url)
         instance.save()
@@ -87,21 +97,35 @@ class Deployer(appier.Observable):
         self.run_command(stop)
 
     def has_base(self):
-        return self.run_command("ls %s" % Deployer.BASE_DIRECTORY)
+        cls = self.__class__
+        return self.run_command("ls %s" % cls.BASE_DIRECTORY) == 0
 
     def run_base(self):
+        cls = self.__class__
         if self.has_base(): return
-        base_s = " ".join(Deployer.BASE_PACKAGES)
-        self.run_command("mkdir -p %s" % Deployer.BASE_DIRECTORY)
+        base_path = "%s/%s" % (cls.BASE_DIRECTORY, cls.CONFIG_FILE)
+        data_path = "%s/%s" % (cls.DATA_DIRECTORY, cls.CONFIG_FILE)
+        base_s = " ".join(cls.BASE_PACKAGES)
+        self.run_command("mkdir -p %s" % cls.BASE_DIRECTORY)
+        self.run_command("mkdir -p %s" % cls.DATA_DIRECTORY)
+        self.run_command("ln -s %s %s" % (base_path, data_path))
         self.run_command("apt-get update")
         self.run_command("apt-get -y install %s" % base_s)
 
+    def run_config(self):
+        cls = self.__class__
+        items = self.environment.items()
+        base_path = "%s/%s" % (cls.BASE_DIRECTORY, cls.CONFIG_FILE)
+        config_s = "\\n".join(["export " + key + "=\\${" + key + "-" + value + "}" for key, value in items])
+        self.run_command("printf \"%s\" > %s" % (config_s, base_path))
+
     def run_script(self, url):
+        cls = self.__class__
         name = url.rsplit("/", 1)[1]
-        self.run_command("rm -rf %s && mkdir -p %s" % (Deployer.TEMP_DIRECTORY, Deployer.TEMP_DIRECTORY))
-        self.run_command("cd %s && wget %s" % (Deployer.TEMP_DIRECTORY, url))
-        self.run_command("cd %s && chmod +x %s && ./%s" % (Deployer.TEMP_DIRECTORY, name, name))
-        self.run_command("rm -rf %s" % Deployer.TEMP_DIRECTORY)
+        self.run_command("rm -rf %s && mkdir -p %s" % (cls.TEMP_DIRECTORY, cls.TEMP_DIRECTORY))
+        self.run_command("cd %s && wget %s" % (cls.TEMP_DIRECTORY, url))
+        self.run_command("cd %s && chmod +x %s && ./%s" % (cls.TEMP_DIRECTORY, name, name))
+        self.run_command("rm -rf %s" % cls.TEMP_DIRECTORY)
 
     def run_command(self, command):
         ssh = self.get_ssh()
@@ -120,7 +144,7 @@ class Deployer(appier.Observable):
             sys.stdout.flush()
             self.trigger("stdout", data)
 
-        return not channel.exit_status_ready()
+        return channel.recv_exit_status()
 
     def get_instance(self):
         return self.instance_c.singleton(address = self.address)
@@ -142,6 +166,10 @@ class Deployer(appier.Observable):
             key_filename = self.id_rsa_path
         )
         return self.ssh
+
+    def close_ssh(self):
+        ssh = self.get_ssh()
+        ssh.close()
 
     def _to_absolute(self, base, url):
         is_absolute = url.startswith("http://") or url.startswith("https://")
