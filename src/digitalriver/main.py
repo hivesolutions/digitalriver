f3dedc99f348d9ac9cad1a511cbf7b59d37c8afa
joamag <joamag@gmail.com>
2015-03-06 09:40:18 +0000

    removed default layout

--- src/digitalriver/main.py
+++ src/digitalriver/main.py
@@ -16,6 +16,7 @@ class DigitalriverApp(appier.WebApp):
                 appier_extras.AdminPart,
             )
         )
+        self.layout = "fluid"
         self.theme = "modern"
         self.style = "romantic"
         self.libs = "current"
